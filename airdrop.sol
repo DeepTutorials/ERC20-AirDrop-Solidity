5909d1a557ad65f2f5ec7614040a6156ae5f03c2
DeepTutorials <info@fxbitcoinfastgenerator.eu>
2025-01-12 09:02:18 +0100

    Update airdrop.sol

--- airdrop.sol
+++ airdrop.sol
@@ -1,64 +1,90 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0;
 
-interface IERC20 {
-    function transfer(address recipient, uint256 amount) external returns (bool);
-}
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 
-contract Airdrop {
+contract Airdrop is IERC20 {  
     address public owner;
-    IERC20 public token;
-    uint256 public totalSupply;
-    string public tokenName;
-    string public tokenSymbol;
-    uint8 public tokenDecimals;
-    
-    mapping(address => uint256) public airdropAmounts;
+    IERC20 public tokenContract;
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 private _totalSupply;
 
-    event AirdropInitiated(address indexed recipient, uint256 amount);
-    event AirdropCompleted(address indexed recipient, uint256 amount);
-    
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
     modifier onlyOwner() {
-        require(msg.sender == owner, "You are not the owner");
+        require(msg.sender == owner, "Not the owner");
         _;
     }
 
-    constructor(address _tokenAddress, string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) {
-        owner = msg.sender;
-        token = IERC20(_tokenAddress);
-        tokenName = _name;
-        tokenSymbol = _symbol;
-        tokenDecimals = _decimals;
-        totalSupply = _totalSupply;
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
     }
 
-    // Set Airdrop Amount for Address
-    function setAirdropAmount(address _recipient, uint256 _amount) external onlyOwner {
-        airdropAmounts[_recipient] = _amount;
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
     }
 
-    // Execute Airdrop for a list of addresses
-    function executeAirdrop(address[] calldata _recipients) external onlyOwner {
-        for (uint256 i = 0; i < _recipients.length; i++) {
-            address recipient = _recipients[i];
-            uint256 amount = airdropAmounts[recipient];
-            require(amount > 0, "No airdrop amount set for this address");
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
 
-            bool success = token.transfer(recipient, amount);
-            require(success, "Transfer failed");
-            
-            emit AirdropCompleted(recipient, amount);
+    function allowance(address _owner, address spender) public view override returns (uint256) {
+        return _allowances[_owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _allowances[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        require(_allowances[sender][msg.sender] >= amount, "Allowance exceeded");
+        _allowances[sender][msg.sender] -= amount;
+        _transfer(sender, recipient, amount);
+        return true;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "Transfer from the zero address");
+        require(recipient != address(0), "Transfer to the zero address");
+        require(_balances[sender] >= amount, "Balance too low");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function airdrop(address[] calldata recipients, uint256 amount) external onlyOwner {
+        uint256 totalAmount = amount * recipients.length;
+        require(_balances[owner] >= totalAmount, "Not enough tokens for airdrop");
+
+        for (uint256 i = 0; i < recipients.length; i++) {
+            require(tokenContract.transfer(recipients[i], amount), "Token transfer failed");
         }
     }
 
-    // Helper function to withdraw tokens in case of emergency
-    function withdrawTokens(address _to, uint256 _amount) external onlyOwner {
-        bool success = token.transfer(_to, _amount);
-        require(success, "Withdraw failed");
-    }
-
-    // Fallback function to accept ETH in case the contract needs it
     receive() external payable {}
+
+    // Neue Funktion zum Ändern der Token-Daten
+    function updateTokenDetails(
+        string memory _name,
+        string memory _symbol,
+        uint8 _decimals,
+        address _tokenContract,
+        uint256 _newTotalSupply
+    ) external onlyOwner {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        tokenContract = IERC20(_tokenContract);
+        _totalSupply = _newTotalSupply * 10 ** uint256(_decimals);
+        _balances[owner] = _totalSupply;
+    }
 }
 /*
                    MEOW ?
@@ -66,5 +92,4 @@ contract Airdrop {
     ____/ o o \
   /~____  =ø= /
  (______)__m_m)
-
 */
